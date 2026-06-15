9d424e38071bea34105f6b773df7cc295fe4c2e7
ankitsendwishonline <ankit@sendwishonline.com>
2026-06-15 11:49:34 +0530

    Add axios for API calls and implement menu type fetching in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Child1 from './home/Child1'
+import { baseUrl } from '../store/axiosinstance'
 
 const Home = () => {
 
+    const [menuTypes, setMenuTypes] = useState([])
+
+    const getMenuType = async () => {
+        try {
+            const response = await baseUrl.get('/menu/type')
+            setMenuTypes(response.data?.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getMenuType();
+    }, [])
+
+    console.log(menuTypes, "menuTypes")
     return (
         <div>
-            <Child1 />
+            {
+                menuTypes.map((item, idx) => (
+                    <div key={idx}>
+                        {item.name}
+                    </div>
+                ))
+            }
         </div>
     )
 }
