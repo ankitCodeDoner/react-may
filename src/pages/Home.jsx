bf44534652943ebff09a517e27e62c281cd7bd4a
ankitsendwishonline <ankit@sendwishonline.com>
2026-06-16 11:42:56 +0530

    Refactor Home component to remove API call and simplify menu type handling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,16 @@
-import React, { useEffect, useState } from 'react'
-import Child1 from './home/Child1'
-import { baseUrl } from '../store/axiosinstance'
+import React, { useEffect } from 'react'
 
 const Home = () => {
 
-    const [menuTypes, setMenuTypes] = useState([])
-
-    const getMenuType = async () => {
-        try {
-            const response = await baseUrl.get('/menu/type')
-            setMenuTypes(response.data?.data)
-        } catch (error) {
-            console.log(error)
-        }
+    const getMenuType = () => {
+        console.log("first")
     }
-
     useEffect(() => {
-        getMenuType();
+        getMenuType()
     }, [])
 
-    console.log(menuTypes, "menuTypes")
     return (
-        <div>
-            {
-                menuTypes.map((item, idx) => (
-                    <div key={idx}>
-                        {item.name}
-                    </div>
-                ))
-            }
-        </div>
+        <div>Home</div>
     )
 }
 
